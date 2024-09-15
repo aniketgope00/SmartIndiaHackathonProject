40b8b8baf98013ffe37dfa04486bd00d1ac443c2
aniketgope00 <aniketgope00@gmail.com>
2024-09-15 19:45:43 +0530

    website is finished and main is cleaned

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+import pandas as pd
 
 app = Flask(__name__,
             template_folder='templates',
@@ -10,6 +11,10 @@ def home():
 
 @app.route('/vision-env')
 def vision_env():
+
+    data = pd.read_csv("object_detection_data.csv")
+    data = data.values.tolist()
+
     # Define YouTube video IDs
     youtube_ids = {
         'camera1': '3qO3sxOtPMc',
@@ -18,11 +23,15 @@ def vision_env():
         'camera4': 'HAX3y6femFQ',
         'camera5': 'CZ6NoakNc8s'
     }
-    return render_template('vision.html', youtube_ids=youtube_ids)
+    return render_template('vision.html', youtube_ids=youtube_ids, data = data)
 
 @app.route('/rl-env')
 def rl_env():
     return render_template('rl.html')
+
+@app.route("/aboutus")
+def about_us():
+    return render_template('aboutus.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
